f09809b2cc87978066b59ec44e12f50ed64b7c11
xah69 <102232889+xah69@users.noreply.github.com>
2022-03-26 10:04:11 +0530

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo mv run.sh "/usr/local/bin"
+sudo mv install_dependencies.sh "/usr/local/bin"
+sudo mv uninstall_dependencies.sh "/usr/local/bin"
+sudo mv uninstaller.sh "/usr/local/bin"
+sudo mv init.py "/usr/local/bin"
+sudo mv exit.py "/usr/local/bin"
+sudo mv installer.sh "/usr/local/bin"
+sudo mv fdk.py "/usr/local/bin"
