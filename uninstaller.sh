e6b2b84efe77be4b861c9cd71ead67e98009174f
xah69 <102232889+xah69@users.noreply.github.com>
2022-03-26 10:02:57 +0530

    Add files via upload

---
+++ uninstaller.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo rm "/usr/local/bin/run.sh"
+sudo rm "/usr/local/bin/install_dependencies.sh"
+sudo rm "/usr/local/bin/uninstall_dependencies.sh"
+sudo rm "/usr/local/bin/installer.sh"
+sudo rm "/usr/local/bin/init.py"
+sudo rm "/usr/local/bin/exit.py"
+sudo rm "/usr/local/bin/fdk.py"
+sudo rm "/usr/local/bin/uninstaller.sh"
+
